64be37e1c4c4d77b819926457d77a32a88ba788c
Javier Aramayo <javier.aramayo@ayi.group>
2021-04-27 20:43:05 -0300

    Exercise 4 resolved

--- clase4/index.js
+++ clase4/index.js
@@ -1,5 +1,5 @@
 // const carBrands = new Array('Chevrolet', 'Ford', 'BMW', 'Audi', 'Fiat')
-const carBrands = ['Chevrolet', 'Ford', 'BMW', 'Audi', 'Fiat']
+// const carBrands = ['Chevrolet', 'Ford', 'BMW', 'Audi', 'Fiat']
 
 // console.log(carBrands)
 // carBrands.push('Ferrari')
@@ -51,11 +51,11 @@ const carBrands = ['Chevrolet', 'Ford', 'BMW', 'Audi', 'Fiat']
 // console.log(carBrands)
 // console.log(filteredCarBrands)
 
-const products = [
-  { id: 0, name: 'Samsung galaxy s20 plus', price: 900, offerPrice: null },
-  { id: 1, name: 'Motorola moto G 8', price: 650, offerPrice: 560 },
-  { id: 2, name: 'Xiaomi Mi 10', price: 750, offerPrice: 500 },
-]
+// const products = [
+//   { id: 0, name: 'Samsung galaxy s20 plus', price: 900, offerPrice: null },
+//   { id: 1, name: 'Motorola moto G 8', price: 650, offerPrice: 560 },
+//   { id: 2, name: 'Xiaomi Mi 10', price: 750, offerPrice: 500 },
+// ]
 
 // Reglas: Si el precio de oferta es null quiero la variable offer en false, si el precio es mayor a 700 el producto tiene envío gratis
 
@@ -123,18 +123,18 @@ const products = [
 // console.log(functionScope) // ERROR PORQUE NO TENEMOS ACCESO
 // console.log(blockScope) // ERROR PORQUE NO TENEMOS ACCESO
 
-const values = [
-  'Text',
-  98.5,
-  true,
-  75,
-  { id: 0, name: 'Object' },
-  960,
-  'Another text',
-  false,
-  76,
-  134,
-]
+// const values = [
+//   'Text',
+//   98.5,
+//   true,
+//   75,
+//   { id: 0, name: 'Object' },
+//   960,
+//   'Another text',
+//   false,
+//   76,
+//   134,
+// ]
 
 // const numberValues = values.filter((value) => !isNaN(parseaccumulator + currentValueInt(value)))
 // const notNumberValues = values.filter((value) => isNaN(parseInt(value)))
@@ -145,11 +145,54 @@ const values = [
 // console.log(notNumberValues)
 // console.log(sumNumberValues)
 
-const sumNumberValues = values
-  .filter((value) => !isNaN(parseInt(value)))
-  .reduce((accumulator, currentValue) => accumulator + currentValue)
+// const sumNumberValues = values
+//   .filter((value) => !isNaN(parseInt(value)))
+//   .reduce((accumulator, currentValue) => accumulator + currentValue)
 
-const notNumberValues = values.filter((value) => isNaN(parseInt(value)))
+// const notNumberValues = values.filter((value) => isNaN(parseInt(value)))
 
-console.log(notNumberValues)
-console.log(sumNumberValues)
+// console.log(notNumberValues)
+// console.log(sumNumberValues)
+
+// Resolución Ejercicio 4
+
+const isNotZero = (value) => parseInt(value) !== 0
+
+const deleteElements = (arrayValues) => {
+  const values = [...arrayValues]
+
+  const module = values.length % 3
+
+  if (module > 0) values.shift()
+  if (module > 1) values.pop()
+
+  return values
+}
+// function isNotZero(value) {
+//   if(parseInt(value) !== 0) {
+//     return true
+//   } else {
+//     return false
+//   }
+// }
+
+let value
+const values = []
+
+while (isNotZero(value)) {
+  value = prompt('Ingrese un valor')
+  if (isNotZero(value)) values.push(value)
+}
+
+const cleanedValues = deleteElements(values)
+const elementsNumber = cleanedValues.length / 3
+
+const arrayOne = cleanedValues.slice(0, elementsNumber)
+const arrayTwo = cleanedValues.slice(elementsNumber, elementsNumber * 2)
+const arrayThree = cleanedValues.slice(elementsNumber * 2, elementsNumber * 3)
+
+console.log(values)
+console.log(cleanedValues)
+console.log(arrayOne)
+console.log(arrayTwo)
+console.log(arrayThree)
